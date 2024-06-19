1765ac610557c1acbf372639cf5dac15b1511cbf
fardin <vahdatfardin@gmail.com>
2024-06-19 15:52:46 +0330

    init sidebar and header

--- components/Base/Sidebar.vue
+++ components/Base/Sidebar.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="h-screen w-[224px] bg-Gray-b2 hidden lg:block">
+    <div class="h-screen min-w-[224px] max-w-[224px] w-full bg-Gray-b2 hidden lg:block">
         <div class="h-[150px] flex items-center">
             <img src="/logo.svg" />
         </div>
-        <ul class="flex flex-col gap-2 h-full pl-5">
+        <ul class="flex flex-col gap-2 h-[calc(100vh-150px)] pl-5">
             <li v-for="(item, index) in list" :key="index" class=" h-[50px]">
                 <NuxtLink class="h-full w-full  flex items-center gap-3 text-[14px] " :to="item.route"
                     :class="route.fullPath.includes(item.route) ? 'text-Gray-b6 font-medium' : 'text-Gray-b3 font-normal'">
@@ -12,26 +12,39 @@
                         <Icon v-else :name="item.icon"></Icon>
                     </div>
                     {{
-                    item.label }}
+                item.label }}
                 </NuxtLink>
             </li>
         </ul>
     </div>
     <div class="lg:hidden block">
         <ClientOnly>
-            <el-drawer v-model="drawer">
+            <el-drawer v-model="drawer" @before-close="drawer = false" lock-scroll size="100%" class="bg-Gray-b2">
                 <template #header>
-                    <h4>set title by slot</h4>
+                    <img src="/logo.svg" class="max-w-fit" />
                 </template>
-                test
-                <template #footer>
-                    <div style="flex: auto">
-                        <el-button @click="cancelClick">cancel</el-button>
-                        <el-button type="primary" @click="confirmClick">confirm</el-button>
-                    </div>
-                </template>
+                <ul class="flex flex-col gap-2 h-full pl-5">
+                    <li v-for="(item, index) in list" :key="index" class=" h-[50px]">
+                        <NuxtLink class="h-full w-full  flex items-center gap-3 text-[14px] " @click="drawer = false"
+                            :to="item.route"
+                            :class="route.fullPath.includes(item.route) ? 'text-Gray-b6 font-medium' : 'text-Gray-b3 font-normal'">
+                            <div class="h-full w-[20px] flex items-center">
+                                <Icon v-if="route.fullPath.includes(item.route)" :name="item.icon + '-active'"></Icon>
+                                <Icon v-else :name="item.icon"></Icon>
+                            </div>
+                            {{
+                item.label }}
+                        </NuxtLink>
+                    </li>
+                </ul>
             </el-drawer>
         </ClientOnly>
+        <div class="flex items-center justify-between w-full py-4  h-[60px]">
+            <img src="/logo.svg" />
+            <button @click="drawer = true" class="mr-6">
+                <Icon name="menu" />
+            </button>
+        </div>
     </div>
 </template>
 
@@ -45,3 +58,16 @@ const route = useRoute()
 
 const drawer = ref(false)
 </script>
+<style lang="scss">
+.el-drawer__header {
+    justify-content: space-between;
+}
+
+.el-drawer__close-btn {
+    max-width: fit-content;
+}
+
+.el-drawer {
+    @apply bg-Gray-b2
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
-    <div class="flex gap-3">
+    <div class="flex lg:gap-3 lg:flex-row flex-col">
         <BaseSidebar />
-        <slot />
+        <div class="lg:max-h-screen max-h-[calc(100vh-60px)] lg:overflow-y-auto overflow-x-auto w-full p-4">
+            <slot />
+        </div>
     </div>
 </template>
 
