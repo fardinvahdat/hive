28d7b161b1e0b1c80a2f62b22b6e971b2d4b0d4a
fardin <vahdatfardin@gmail.com>
2024-06-22 12:50:58 +0330

    update

--- components/TheDashboard/PaperTrading.vue
+++ components/TheDashboard/PaperTrading.vue
@@ -114,6 +114,116 @@
                 </div>
             </div>
         </div>
+        <div class="my-6 p-8 rounded-lg bg-Gray-b2">
+            <div class="flex justify-between items-center border-b-[1px] border-Gray-b1 pb-4">
+                <p class="text-base font-normal">Name of Channel</p>
+                <el-dropdown trigger="click">
+                    <span class="el-dropdown-link">
+                        <Icon name="more" />
+                    </span>
+                    <template #dropdown>
+                        <el-dropdown-menu>
+                            <el-dropdown-item class="flex items-center gap-1">
+                                <Icon name="visibility" />
+                                <span>Show Detail</span>
+                            </el-dropdown-item>
+                            <el-dropdown-item class="flex items-center gap-1">
+                                <Icon name="folder-up" />
+                                <span>UnSubscribe</span>
+                            </el-dropdown-item>
+                        </el-dropdown-menu>
+                    </template>
+                </el-dropdown>
+            </div>
+            <div class="mt-6 grid grid-cols-4 items-center gap-6">
+                <div class="lg:col-span-3 col-span-4">
+                    <el-collapse class="block lg:hidden">
+                        <el-collapse-item>
+                            <template #title>Filters</template>
+                            <div class="flex flex-wrap md:flex-row flex-col gap-3">
+                                <div class="min-w-[128px] max-w-[128px] flex flex-col gap-2">
+                                    <label class="text-sm text-Gray-b4 font-normal">Start Balance</label>
+                                    <el-input v-model="filter.balance" placeholder="Start Balance"
+                                        input-style="color:#FFFFFF;font-size:16px" />
+                                </div>
+                                <div class="min-w-[128px] flex flex-col gap-2">
+                                    <label class="text-sm text-Gray-b4 font-normal">Duration</label>
+                                    <ClientOnly>
+                                        <el-date-picker v-model="filter.duration" type="daterange" range-separator="-"
+                                            start-placeholder="Start date" end-placeholder="End date" />
+                                    </ClientOnly>
+                                </div>
+                                <div class="min-w-[128px] max-w-[128px] flex flex-col gap-2">
+                                    <label class="text-sm text-Gray-b4 font-normal">Max Assets</label>
+                                    <el-select v-model="filter.maxAsset" placeholder="Max Assets">
+                                        <el-option v-for="item in options" :key="item.value" :label="item.label"
+                                            :value="item.value" />
+                                    </el-select>
+                                </div>
+                                <div class="min-w-[128px] max-w-[128px] flex flex-col justify-end gap-2">
+                                    <button class="px-2 bg-Blue text-Gray-b6 rounded-lg h-[32px]">Execute</button>
+                                </div>
+                            </div>
+                        </el-collapse-item></el-collapse>
+                    <div class=" flex-wrap md:flex-row flex-col gap-3 lg:flex hidden">
+                        <div class="min-w-[128px] max-w-[128px] flex flex-col gap-2">
+                            <label class="text-sm text-Gray-b4 font-normal">Start Balance</label>
+                            <el-input v-model="filter.balance" placeholder="Start Balance"
+                                input-style="color:#FFFFFF;font-size:16px" />
+                        </div>
+                        <div class="min-w-[128px] flex flex-col gap-2">
+                            <label class="text-sm text-Gray-b4 font-normal">Duration</label>
+                            <ClientOnly>
+                                <el-date-picker v-model="filter.duration" type="daterange" range-separator="-"
+                                    start-placeholder="Start date" end-placeholder="End date" />
+                            </ClientOnly>
+                        </div>
+                        <div class="min-w-[128px] max-w-[128px] flex flex-col gap-2">
+                            <label class="text-sm text-Gray-b4 font-normal">Max Assets</label>
+                            <el-select v-model="filter.maxAsset" placeholder="Max Assets">
+                                <el-option v-for="item in options" :key="item.value" :label="item.label"
+                                    :value="item.value" />
+                            </el-select>
+                        </div>
+                        <div class="min-w-[128px] max-w-[128px] flex flex-col justify-end gap-2">
+                            <button class="px-2 bg-Blue text-Gray-b6 rounded-lg h-[32px]">Execute</button>
+                        </div>
+                    </div>
+                    <div class="grid grid-cols-4 gap-6 mt-6">
+                        <div class="lg:col-span-1 col-span-4">
+                            <div>
+                                <highchart :options="paperTradingChartOptions" />
+                            </div>
+                            <div class="flex flex-col gap-2">
+                                <div class="flex justify-between items-center">
+                                    <span class="text-base text-Gray-b6 font-medium">Balance</span>
+                                    <span class="text-base text-Primary font-medium">1000$</span>
+                                </div>
+                                <div v-for="(item, index) in chartLables" :key="index"
+                                    class="flex items-center justify-between">
+                                    <div class="flex items-center gap-2">
+                                        <img :src="'/icons/' + item.icon + '.svg'"
+                                            class="w-8 h-8 rounded-lg object-fill bg-Gray-b3 p-2 mr-2" />
+                                        <div class="w-2 h-2 rounded-full" :style="`background-color:${item.color}`">
+                                        </div>
+                                        <span class="text-sm font-normal text-Gray-b3">{{ item.label }}</span>
+                                    </div>
+                                    <p class="text-Gray-b6 font-medium text-[14px]">{{ item.value }} %</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="lg:col-span-3 col-span-4">
+                            <div>
+                                <highchart :options="paperTradingChartOptions2" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="lg:col-span-1 col-span-4">
+                    <BaseBalanceChange :state="balanceChanges" />
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
