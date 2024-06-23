24375babb4a02904336fdf978344bfe5ce49497b
fardin <vahdatfardin@gmail.com>
2024-06-23 15:13:20 +0330

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,32 +14,6 @@ export default defineNuxtConfig({
         {
           rel: "apple-touch-startup-image",
           href: "/splash.png",
-          media:
-            "(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
-        },
-        {
-          rel: "apple-touch-startup-image",
-          href: "/splash.png",
-          media:
-            "(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)",
-        },
-        {
-          rel: "apple-touch-startup-image",
-          href: "/splash.png",
-          media:
-            "(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-        },
-        {
-          rel: "apple-touch-startup-image",
-          href: "/splash.png",
-          media:
-            "(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)",
-        },
-        {
-          rel: "apple-touch-startup-image",
-          href: "/splash.png",
-          media:
-            "(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)",
         },
       ],
       meta: [
