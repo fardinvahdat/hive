c8df406ba6cd16eacfbb76673a1dab60388dce49
fardin <vahdatfardin@gmail.com>
2024-06-23 15:15:51 +0330

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,18 +20,6 @@ export default defineNuxtConfig({
         {
           name: "theme-color",
         },
-        {
-          name: "apple-mobile-web-app-capable",
-          content: "yes",
-        },
-        {
-          name: "apple-mobile-web-app-status-bar-style",
-          content: "default",
-        },
-        {
-          name: "apple-mobile-web-app-title",
-          content: "PWA Splash",
-        },
       ],
     },
   },
