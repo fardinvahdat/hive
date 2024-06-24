7aabc87da28ea6478ec5e8434630d772723e1b97
fardin <vahdatfardin@gmail.com>
2024-06-24 17:25:33 +0330

    init breadcrumb component

---
+++ components/Base/Breadcrumb.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex items-center gap-2">
+        <NuxtLink v-for="(item, index) in state" :key="index" :to="item.route"
+            class="font-medium text-[14px] text-Gray-b5 flex items-center gap-2">{{
+            item.label }}
+            <Icon v-if="index !== state.length - 1" name="angle-right" />
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    state: {
+        default: [
+            {
+                label: "",
+                route: ''
+            }
+        ]
+    }
+})
+</script>
